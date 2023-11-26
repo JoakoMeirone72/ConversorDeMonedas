4b33c2272acfc8ead79a4eb8e1b48fdb14f7535c
Joako Meirone <joakomeirone@gmail.com>
2023-11-26 03:46:21 -0300

    creacion dos componentes

---
+++ src/app/components/add-fav/add-fav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-fav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-fav.component.html',
+  styleUrls: ['./add-fav.component.scss']
+})
+export class AddFavComponent {
+
+}
---
+++ src/app/components/delete-fav/delete-fav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-delete-fav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './delete-fav.component.html',
+  styleUrls: ['./delete-fav.component.scss']
+})
+export class DeleteFavComponent {
+
+}
